22b09bda6c5be57e2eaede2bb87ed15a61f0c2c2
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-06-18 17:09:50 +0400

    Create hypermarket-child.js

---
+++ assets/js/hypermarket-child.js
@@ -0,0 +1,11 @@
+/**
+ * Hypermarket Child Theme
+ *
+ * @author      Mahdi Yazdani
+ * @package     Hypermarket Child
+ * @since       1.0.0
+ */
+jQuery(document).ready(function($) {
+    'use strict';
+
+}); /*Document Ready End*/
